cb5dc70fe9338fbaea55541464adb6094e2e4d06
yeissonrol <yeissonrol@gmail.com>
2020-05-05 22:34:34 -0500

    Se sube el proyecto damojava

---
+++ src/main/java/com/demojava1/demojava1/Demojava1Application.java
@@ -0,0 +1,13 @@
+package com.demojava1.demojava1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demojava1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demojava1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/demojava1/demojava1/Demojava1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demojava1.demojava1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demojava1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
